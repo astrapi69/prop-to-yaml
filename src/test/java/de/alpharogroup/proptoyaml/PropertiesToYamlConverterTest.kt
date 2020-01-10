06f01cea4ff0db979ac8693b445a6249d9e156d0
astrapi69 <asterios.raptis@gmx.net>
2020-01-10 18:36:03 +0100

    initial version 2

--- src/test/java/de/alpharogroup/proptoyaml/PropertiesToYamlConverterTest.kt
+++ src/test/java/de/alpharogroup/proptoyaml/PropertiesToYamlConverterTest.kt
@@ -1,21 +1,18 @@
-package de.alpharogroup.proptoyaml;
+package de.alpharogroup.proptoyaml
 
-import de.alpharogroup.file.search.PathFinder;
-import de.alpharogroup.prop.to.yaml.PropertiesToYamlConverter;
-import org.testng.annotations.Test;
+import de.alpharogroup.file.search.PathFinder
+import de.alpharogroup.prop.to.yaml.PropertiesToYamlConverter.convert
+import org.testng.Assert
+import org.testng.annotations.Test
+import java.io.File
 
-import java.io.File;
-
-import static org.testng.Assert.*;
-
-public class PropertiesToYamlConverterTest {
-
+class PropertiesToYamlConverterTest {
     @Test
-    public void testConvertListObjects() {
-        String expected;
-        String actual;
-        File propertiesFile = new File(PathFinder.getSrcTestResourcesDir(), "list-or-array.properties");
-        actual = PropertiesToYamlConverter.convert(propertiesFile);
+    fun testConvertListObjects() {
+        val expected: String
+        val actual: String
+        val propertiesFile = File(PathFinder.getSrcTestResourcesDir(), "list-or-array.properties")
+        actual = convert(propertiesFile)
         expected = "application:\n" +
                 "  property:\n" +
                 "  - first\n" +
@@ -24,24 +21,23 @@ public class PropertiesToYamlConverterTest {
                 "  - first: /v1/first\n" +
                 "    second: /v1/second\n" +
                 "  - first: /v1/public/first\n" +
-                "    second: /v1/public/second\n";
-        assertEquals(actual, expected);
+                "    second: /v1/public/second\n"
+        Assert.assertEquals(actual, expected)
     }
+
     @Test
-    public void testConvert()
-    {
-        String expected;
-        String actual;
-        File propertiesFile = new File(PathFinder.getSrcTestResourcesDir(), "config.properties");
-        actual = PropertiesToYamlConverter.convert(propertiesFile);
+    fun testConvert() {
+        val expected: String
+        val actual: String
+        val propertiesFile = File(PathFinder.getSrcTestResourcesDir(), "config.properties")
+        actual = convert(propertiesFile)
         expected = "application:\n" +
                 "  http:\n" +
                 "    port: '18080'\n" +
                 "  https:\n" +
                 "    port: '18443'\n" +
                 "configuration:\n" +
-                "  type: DEVELOPMENT\n";
-        assertEquals(actual, expected);
+                "  type: DEVELOPMENT\n"
+        Assert.assertEquals(actual, expected)
     }
-
 }
